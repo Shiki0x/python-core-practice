1355a1a701a844ae0fc7e7782aa494f26a27e92d
Shiki <baelor278@gmail.com>
2025-12-11 05:36:52 -0700

    Updated Day 1 bonus: favorite game script

--- day1_bonus.py
+++ day1_bonus.py
@@ -1,5 +1,5 @@
-name = input()
-game = input()
-time_played = input()
+name = input("What is your name? ")
+game = input("What is your favorite game? ")
+time_played = input("How many hours have you played? ")
 
 print("Damn " + name + ", you've played " + game + " for " + time_played + " hours? That's wild.")
